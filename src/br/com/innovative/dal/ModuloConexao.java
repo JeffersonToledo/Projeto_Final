d3478127499146a27ab534ef877d309226c24114
jefftoledo <queir@192.168.15.5>
2020-10-21 10:27:46 -0200

    Login

---
+++ src/br/com/innovative/dal/ModuloConexao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.innovative.dal;
+import java.sql.*;
+
+/**
+ *
+ * @author queir
+ */
+public class ModuloConexao {
+    //método responsável para estabelecer a conexão com o banco de dados
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        // a linha abaixo chama o driver
+        String driver = "com.mysql.jdbc.Driver";
+        // Armazenando informações referente ao Banco de Dados
+        String url="jdbc:mysql://localhost:3306/innovative";
+        String user="root";
+        String password="";
+        // Estabelecendo a conexão com o Banco de Dados
+        try {
+           Class.forName(driver);
+           conexao = DriverManager.getConnection(url,user,password);
+           return conexao;
+        } catch (Exception e) {
+            // A linha abaixo identifica erros
+            // System.out.println(e);
+            return null;
+        }
+    }
+}
